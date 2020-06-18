1bfe4ec4b1b52a69a38afb858a5f8872e87a2bed
michaelArvidsson <michael.arvidsson@gmail.com>
2020-06-18 10:11:57 +0200

    added functions to play

--- app.js
+++ app.js
@@ -48,12 +48,33 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 		document.querySelector('.player-0-panel').classList.toggle('active');
 		document.querySelector('.player-1-panel').classList.toggle('active');
 
-		//document.querySelector('.player-0-panel').classList.remove('active');
-		//document.querySelector('.player-1-panel').classList.add('active');
-
 		diceDOM.style.display = 'none';
 	}
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+	//Add current score to global score
+	scores[activePlayer] += roundScore;
+
+	// Update the UI
+	document.querySelector('#score-' + activePlayer).textContent =
+		scores[activePlayer];
+	// Check if player won the game
+});
+
+function nextPlayer() {
+	//Next player
+	activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	diceDOM.style.display = 'none';
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 /* document.querySelector('#current-' + activePlayer).innerHTML =
